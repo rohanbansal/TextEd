5cf80a585acfb9827324e782589c968198983a10
Rohan <rohan.bansal6@gmail.com>
2016-03-24 12:14:14 -0400

    Added increment totalSent.

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.post('/message', function (req, res) {
 
     // TODO increment total sent here
     usersRef.child(fromNum).update({
-      totalSent: usersDb[fromNum].totalSent + 1
+      totalSent: usersDB[fromNum].totalSent + 1
     })
     res.writeHead(200, {
       'Content-Type':'text/xml'
